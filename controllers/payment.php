8f92c75c20d6c0e19cdf94d0f3de7e5a44071658
root <root@localhost>
2017-06-21 23:45:44 -0400

    corr buyerEmail

--- controllers/payment.php
+++ controllers/payment.php
@@ -15,8 +15,9 @@ class Payment extends MX_Controller {
 
     function index() {
         $account_data = $this->session->userdata("accountinfo");
-        $data = $this->data_base($account_data);
         $system_config = common_model::$global_config['system_config'];
+        $data = $this->data_base($account_data, $system_config);
+
         if($system_config['payulatam_mode'] == 0) {
             $data['testMode'] = false;
             $data['apiKey'] = $system_config['payulatam_apikey'];
@@ -29,20 +30,26 @@ class Payment extends MX_Controller {
             $data['payulatam_url'] = 'https://sandbox.gateway.payulatam.com/ppp-web-gateway/';
         }
 
+        $data['payerPhone'] = $account_data['telephone_1'];
+        $data['payerMobilePhone'] = $account_data['telephone_2'];
+        $data['payerEmail'] = $account_data['email'];
+ 
+        //params need for transaction
         $data["merchantId"] = $system_config['payulatam_merchantid'];
         $data["accountId"] = $system_config['payulatam_accountid'];
         $data["checkValue"] = md5($data['merchantId'].PAYULATAM_SALT);
-        $data["gateway_tax"] = 0;
+        $data["gateway_tax"] = floatval($system_config['payulatam_gatewayTax']);
         $data["from_currency"] = $this->common->get_field_name('currency', 'currency', $account_data["currency_id"]);
         $data["to_currency"] = Common_model::$global_config['system_config']['base_currency'];
         $this->load->view("user_payment",$data);
     }
 
-    private function data_base($account_data) {
+    private function data_base($account_data, $system_config) {
+
         return array(
             'customerId' => $account_data['id'],
-            'buyerEmail' => 'dirindesa@neurotec.c',
-            'buyerFullName' => 'Neurotec Tecnologia S.A.S',
+            'buyerEmail' => $account_data['email'],
+            'buyerFullName' => $account_data['company_name'],
             'description' => 'Refill account ' . $account_data['number']
         );
     }
